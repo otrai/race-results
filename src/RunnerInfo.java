cd0043f6f249397a9958d5cb59c5fcf613b6db37
Matthew Fawcett <107450303+otrai@users.noreply.github.com>
2024-06-22 03:31:54 -0700

    Added 2 src files and ignored project files in the .gitignore.

---
+++ src/RunnerInfo.java
@@ -0,0 +1,17 @@
+class RunnerInfo {
+    private int timeRun;
+    private double distRun;
+
+    public void setTime(int timeRunSecs) {     // Time run in seconds
+        timeRun = timeRunSecs;                   // timeRun refers to the field
+    }
+
+    public void setDist(double distRunMiles) {  // Distance run in miles
+        distRun = distRunMiles;
+    }
+
+    public double getSpeedMph() {               // Speed in miles/hour
+        return distRun / (timeRun / 3600.0);     // miles / (secs / (secs / hour))
+    }
+}
+
